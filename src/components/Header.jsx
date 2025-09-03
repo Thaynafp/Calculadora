9831d4bb4fdc34aa9cc2d9f82281802de37fd46b
Thaynafp <tflopes01@gmail.com>
2025-09-03 17:20:29 -0300

    adicionando header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "../css/header.css"
+
+function Header(){
+    return(
+        <div className="header">
+            <h1>Calculadora Digital</h1>
+        </div>
+    )
+}
+
+export default Header 
